d4d8403e187b139d31b15327358b62ccd6524ab6
danielkim428 <kim.sunk@northeastern.edu>
2022-09-20 18:25:00 -0400

    Only Homework 1.rkt works

--- Homework 1.rkt
+++ Homework 1.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |CS2500 HW1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname |Homework 1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/image)
 (require 2htdp/universe)
 
@@ -49,33 +49,30 @@
 ;; play : World -> World
 ;; Uses big-bang to play a game of Spelling Bee
 (define (play w)
-  (big-bang w
-    [to-draw world->image]
-    [on-key key-pressed]))
+  (big-bang w [to-draw world->image] [on-key key-pressed]))
 
 ;; world->image : World -> Image
 ;; Consumes a world and outputs an image of the world
 (define (world->image w)
-  (beside (overlay 
-           (above (text (world-pw w) 40 "black")
-                  (letters->image (world-l w)))
-           (rectangle 400 500 "solid" "white"))
-          (overlay/align/offset
-           "middle" "top"
-           (prev-list->image (world-prev w))
-           0 -30
-           (rectangle 400 500 "solid" "white"))))
+  (beside (overlay (above (text (world-pw w) 40 "black") (letters->image (world-l w)))
+                   (rectangle 400 500 "solid" "white"))
+          (overlay/align/offset "middle"
+                                "top"
+                                (prev-list->image (world-prev w))
+                                0
+                                -30
+                                (rectangle 400 500 "solid" "white"))))
 
 (check-expect (world->image WORLD-1)
-              (beside (overlay 
-                       (above (text (world-pw WORLD-1) 40 "black")
-                              (letters->image (world-l WORLD-1)))
-                       (rectangle 400 500 "solid" "white"))
-                      (overlay/align/offset
-                       "middle" "top"
-                       (prev-list->image (world-prev WORLD-1))
-                       0 -30
-                       (rectangle 400 500 "solid" "white"))))
+              (beside (overlay (above (text (world-pw WORLD-1) 40 "black")
+                                      (letters->image (world-l WORLD-1)))
+                               (rectangle 400 500 "solid" "white"))
+                      (overlay/align/offset "middle"
+                                            "top"
+                                            (prev-list->image (world-prev WORLD-1))
+                                            0
+                                            -30
+                                            (rectangle 400 500 "solid" "white"))))
 
 ;; letters->image : Letters -> Image
 ;; It consumes letters and produces an image of corresponding letter-status
@@ -84,30 +81,31 @@
     [(empty? (rest alist))
      (overlay (overlay (text (first alist) 20 "black") (circle 30 "solid" (color 245 218 73)))
               (square 350 "solid" "white"))]
-    [(cons? alist) (overlay/offset (overlay (text (first alist) 20 "black") (circle 30 "solid" (color 230 230 230)))
-                                   (if (positive? (- 7 (length (rest alist))))
-                                       (* 65 (cos (* (/ (* 2 pi) 6) (length (rest alist)))))
-                                       (* 130 (cos (* (/ (* 2 pi) 12) (length (rest alist))))))
-                                   (if (positive? (- 7 (length (rest alist))))
-                                       (* 65 (sin (* (/ (* 2 pi) 6) (length (rest alist)))))
-                                       (* 130 (sin (* (/ (* 2 pi) 12) (length (rest alist))))))
-                                   (letters->image (rest alist)))]))
+    [(cons? alist)
+     (overlay/offset (overlay (text (first alist) 20 "black") (circle 30 "solid" (color 230 230 230)))
+                     (if (positive? (- 7 (length (rest alist))))
+                         (* 65 (cos (* (/ (* 2 pi) 6) (length (rest alist)))))
+                         (* 130 (cos (* (/ (* 2 pi) 12) (length (rest alist))))))
+                     (if (positive? (- 7 (length (rest alist))))
+                         (* 65 (sin (* (/ (* 2 pi) 6) (length (rest alist)))))
+                         (* 130 (sin (* (/ (* 2 pi) 12) (length (rest alist))))))
+                     (letters->image (rest alist)))]))
 
 (check-expect (letters->image (list "A"))
-              (overlay (overlay (text "A" 20 "black")
-                                (circle 30 "solid" (color 245 218 73)))
+              (overlay (overlay (text "A" 20 "black") (circle 30 "solid" (color 245 218 73)))
                        (square 350 "solid" "white")))
 
 (check-expect (letters->image (list "A" "B" "C"))
-              (overlay/offset (overlay (text "A" 20 "black") (circle 30 "solid" (color 230 230 230)))
-                              (* 65 (cos (* (/ (* 2 pi) 6) 2)))
-                              (* 65 (sin (* (/ (* 2 pi) 6) 2)))
-                              (overlay/offset (overlay (text "B" 20 "black") (circle 30 "solid" (color 230 230 230)))
-                                              (* 65 (cos (* (/ (* 2 pi) 6) 1)))
-                                              (* 65 (sin (* (/ (* 2 pi) 6) 1)))
-                                              (overlay (overlay (text "C" 20 "black")
-                                                                (circle 30 "solid" (color 245 218 73)))
-                                                       (square 350 "solid" "white")))))
+              (overlay/offset
+               (overlay (text "A" 20 "black") (circle 30 "solid" (color 230 230 230)))
+               (* 65 (cos (* (/ (* 2 pi) 6) 2)))
+               (* 65 (sin (* (/ (* 2 pi) 6) 2)))
+               (overlay/offset
+                (overlay (text "B" 20 "black") (circle 30 "solid" (color 230 230 230)))
+                (* 65 (cos (* (/ (* 2 pi) 6) 1)))
+                (* 65 (sin (* (/ (* 2 pi) 6) 1)))
+                (overlay (overlay (text "C" 20 "black") (circle 30 "solid" (color 245 218 73)))
+                         (square 350 "solid" "white")))))
 
 (check-expect (letters->image (list "A" "B" "C" "D" "E" "F" "G" "H"))
               (overlay/offset (overlay (text "A" 20 "black") (circle 30 "solid" (color 230 230 230)))
@@ -120,8 +118,7 @@
 (define (prev-list->image prev)
   (cond
     [(empty? prev) (rectangle 1 1 "solid" "white")]
-    [(cons? prev) (above (text (first prev) 18 "black")
-                         (prev-list->image (rest prev)))]))
+    [(cons? prev) (above (text (first prev) 18 "black") (prev-list->image (rest prev)))]))
 
 (check-expect (prev-list->image '()) (rectangle 1 1 "solid" "white"))
 (check-expect (prev-list->image (list "hi"))
@@ -147,8 +144,7 @@
 (define (key-in-letters l k)
   (cond
     [(empty? (rest l)) (string=? (first l) (string-upcase k))]
-    [(cons? l) (or (string=? (first l) (string-upcase k))
-                   (key-in-letters (rest l) k))]))
+    [(cons? l) (or (string=? (first l) (string-upcase k)) (key-in-letters (rest l) k))]))
 
 (check-expect (key-in-letters LETTERS-1 "o") #true)
 (check-expect (key-in-letters LETTERS-1 "x") #false)
